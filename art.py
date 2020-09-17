cb81a54c739c632c58afcc38509e13933b9b4566
naturkach <naturkach_ai@ukr.net>
2020-09-17 18:12:45 +0300

    added function domagic

--- art.py
+++ art.py
@@ -3,15 +3,17 @@
 import datetime
 
 #day of the week as an integer, where Monday is 0 and Sunday is 6.
-dayn = datetime.datetime.today().weekday()
-print "day:", dayn
-if (dayn < 6):
-	dayn = dayn +2
-#	nexd = dayn +2
-else: 
-	dayn = 0
-#	nexd = 1
+pyday = datetime.datetime.today().weekday()
+print "pyday:", pyday
 
+if (pyday <= 5):
+	daynow = pyday +2
+	nexday = daynow +1
+else: 
+	daynow = 1
+	nexday = 2
+
+print "daynow:", daynow
 #f    = open("pacman", "r")
 #line = f.readlines()
 #curr = line[dayn]
@@ -19,54 +21,53 @@ else:
 with open('pacman', 'r') as f1, open('pacman.tmp' , 'w') as f2:
 	lines = f1.readlines()
 #	curr  = lines[dayn]
-	poz = 10 #deff value
-	strn= 0
-		
-	for line in lines:
-		strn = strn + 1 
-		line = line.strip()
-		#print (strn, line)
-		curr = line[dayn]
-		#print ('curr', curr)	
-		if (dayn == strn):
-			print strn, line, "today"
-		else:
-			print strn, line
-
-#		f2.write(line)
-
-
+	poz = 0 #deff value
+#	strn= 0
 	
-		if   "v" in line:
-			print("ok")
-			poz 	= curr.find("v")	
-			print "poz:", poz
-			oldchar = "x"
+
+	def domagic():
+            #print "oldchar", oldchar
+	    next_day_char = line[nexday][poz]
+	    if  (next_day_char == '.'):
+                nextchar = "n"
+            elif(next_day_char == 'x'):
+		nextchar = "v"
+	   	   
+	    newline_list  	  = list(lines[(nexday-1)])
+	    #print "nextdayl", newline_list
+	    newline_list[poz]     = nextchar
+	    newstr 		  = "".join(newline_list)	    
+	    print newstr
+	   # nl = line[nexd].replace('.','n',poz+1)
+
+	for line in lines:
+		line = line.strip()
+		curr = line[daynow]		
+		#print ('curr', curr)	
+
+		if "v" in line and daynow !=7:
+			poz = line.find("v")				
+			oldchar = "x"		
+		        oldline = line.replace('v', 'x')
+			print oldline			
+           		domagic()
 		elif "n" in line:
-			print("no")
-			poz = curr.find("n")
+#			print("no")
+			poz = line.find("n")
 			oldchar = "."
+			oldline = line.replace('n', '.')
+		elif (line == daynow + 1):
+			continue
+		else:
+		    print line
+		
 
-#get nextday char:
-		next_day_char = line[nexd][poz]
-		if  (next_day_char == '.'):
-#	nl = line[nexd].replace('.','n',poz+1)
-			nextact = "n"
-		elif(next_day_char == 'x'):
-			nextact = "v"
-#	
-		newline_list  	  = list(line[nexd])
-		newline_list[poz] = nextact
-		newstr 		  = "".join(newline_list)
 
-		oldline_list	  = list(line[dayn])
-		oldline_list[poz] = oldchar
-		oldstr		  = "".join(oldline_list)
 #curr[poz] = "z"
-		print "today ", curr
-		print "nextd ", newstr
-		print "old   ", oldstr
-		f2.write(oldstr)
+#		print "today ", curr
+#		print "nextd ", newstr
+#		print "old   ", oldstr
+#		f2.write(oldstr)
 
 
 #change current
