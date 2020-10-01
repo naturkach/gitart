66a84d6251a839fcc01ccb67195ec41df5d86e19
naturkach <naturkach_ai@ukr.net>
2020-10-01 17:45:21 +0300

    fixed sysntax

--- art.py
+++ art.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 import os
+from git import Repo
 
 #day of the week as an integer, where Monday is 0 and Sunday is 6.
 #bat:
@@ -10,7 +11,7 @@ import os
 # 7123456 - git_day
 
 pyday = datetime.datetime.today().weekday()
-print "pyday:", pyday
+print ("pyday:", pyday)
 
 if (pyday < 6):
 	daynow = pyday + 1
@@ -19,78 +20,87 @@ else:
 	daynow = pyday + 1
 	nexday = 1
 
-print "daynow:", daynow
+print ("daynow:", daynow)
+print ("nexday:", nexday)
 
 if os.path.exists("pacman.tmp"):
     os.remove("pacman.tmp")
 
-#f    = open("pacman", "r")
-#line = f.readlines()
-#curr = line[dayn]
+commit = "no"
+
+PATH_OF_GIT_REPO = r'home/naturkach/gitart'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code') 
+
 
 with open('pacman', 'r') as f1, open('pacman.tmp' , 'w') as f2:
-	lines = f1.readlines()
-	poz = 0 #deff value
-	linenumb = 0	
+    lines = f1.readlines()
+    poz = 0 #deff value
+    linenumb = 1	
 
-	def domagic():
-	    print "--nxday--", lines[(nexday)]
-	    print "--nd2--", line
-	    next_day_char = line[nexday][poz]
+    def domagic():
+        next_day_char = line[nexday][poz]
 	   
-	    if  (next_day_char == '.'):
-                nextchar = "n"
-            elif(next_day_char == 'x'):
-		nextchar = "v"
-	   	   
-	    newline_list  	  = list(lines[(nexday)])
-	    newline_list[poz]     = nextchar	    
-	    newstr = "".join(newline_list)
-	    newstr = newstr.rstrip()	    	    
-	    print (newstr)
-	    f2.write(newstr)
-	    f2.write("\n")
+        if  (next_day_char == '.'):
+            nextchar = "n"
+        elif(next_day_char == 'x'):
+            nextchar = "v"
 
-	for line in lines:
-		line = line.strip()
-		curr = line[daynow]		
-		#print ('curr', curr)	
+    newline_list  	  = list(lines[(nexday)])
+    newline_list[poz]     = nextchar	    
+    newstr = "".join(newline_list)
+    newstr = newstr.rstrip()	    	    
+    print (linenumb, newstr)
+    f2.write(newstr)
+    f2.write("\n")
 
-		if "v" in line and daynow !=7:
-			poz = line.find("v")				
-			oldchar = "x"		
-		        oldline = line.replace('v', 'x')
-			print oldline
-			f2.write(oldline)
-			f2.write("\n")					
-           		domagic()
-			linenumb += 2			
-		elif "n" in line:
-			poz = line.find("n")
-			oldchar = "."
-			oldline = line.replace('n', '.')
-			linenumb += 2
-			print linenumb
-		elif ( linenumb == nexday ):
-		    linenumb += 1
-		else:
-		    print line
-	            f2.write(line)
-		    f2.write("\n")
-		    linenumb += 1
+    for line in lines:
+        line = line.strip()
+        curr = line[daynow]		
+
+        if "v" in line and daynow !=7:
+            poz = line.find("v")				
+            oldchar = "x"		
+            oldline = line.replace('v', 'x')
+            print (linenumb, oldline)
+            linenumb + 1
+            f2.write(oldline)
+            f2.write("\n")					
+            domagic()
+            linenumb += 1
+            commit = "yes"
+			
+		#	print "line-",linenumb			
+        elif "n" in line:
+            poz = line.find("n")
+            oldchar = "."
+            oldline = line.replace('n', '.')
+            linenumb += 2
+	    #rint linenumb
+            commit = "no"
+        elif ( linenumb == nexday+1 ):
+            linenumb += 1
+#	    print "lnub = nextd", linenumb, nexday
+   		
+        else:	       
+            print (linenumb, line)
+            f2.write(line)
+            f2.write("\n")
+            linenumb += 1
 		
 
 f1.close()
 f2.close()
 
-#curr[poz] = "z"
-#		print "today ", curr
-#		print "nextd ", newstr
-#		print "old   ", oldstr
-#		f2.write(oldstr)
-
-
-#change current
-
-#print "dayn", dayn, line[dayn]
-#print "next", nexd, line[nexd], ndc
+if (commit == "yes"):
+    git_push()
+    
